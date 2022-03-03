2a50e69260f54138514ed6239f43754142e50ca4
zhao8821276 <779265288@qq.com>
2022-03-03 19:42:09 +0800

    66666#all

--- test_name_zi.py
+++ test_name_zi.py
@@ -1,1 +1,13 @@
+import unittest
 from selenium import webdriver
+class Test_name(unittest.TestCase):
+    def setUp(self):
+        self.name = webdriver.Firefox()
+    def tearDown(self):
+        self.name.quit()
+    def test_name_indux(self):
+        self.name.get('http://127.0.0.1:8000')
+        self.assertIn('worked',self.name.title)
+        self.fail('hahaha')
+if __name__ == '__main__':
+    unittest.main()
